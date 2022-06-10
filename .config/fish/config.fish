457e85479ebc085de10aeb554fed26d98a954e7f
Davi de Castro Reis <davi.reis@gmail.com>
2022-06-09 23:42:56 -0300

    Testing yadm

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set -Ux theme_display_date no
+
+alias fd=fdfind
+alias bat=batcat
